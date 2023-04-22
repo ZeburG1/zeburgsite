1a0e64adcd83a4cd824816b44ba51661247dc847
Gogrmogr1 <gogrmogr@gmail.com>
2023-04-22 15:55:40 +0500

    first

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
     <div class="footer-content">
         <a class="footer-icons" href="https://github.com/ZeburG1"><img class="icon" src="../src/assets/icons/github-icon.webp" alt="MyGithub"></a>
         <a class="footer-icons" href="https://discordapp.com/users/299807775326732288/"><img class="icon" src="../src/assets/icons/discord-icon.webp"  alt="MyDiscord"></a>
-        <img style="width: 0px; height: 0px;" src="../public/manifest.png"> 
+        <img style="width: 0px; height: 0px;" src="../public/icon_256x256.png"> 
     </div>
 </footer>
 </template>
